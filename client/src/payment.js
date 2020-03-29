f712f71463819dc71efc0e93e73b9753b80db2ef
Markos Georghiades <53157953+Markos-The-G@users.noreply.github.com>
2020-03-28 23:51:15 -0400

    changed smart contract

---
+++ client/src/payment.js
@@ -0,0 +1,6 @@
+import web3 from './web3';
+
+const address = '0xb84b12e953f5bcf01b05f926728e855f2d4a67a9';
+
+
+export default new web3.eth.Contract(abi, address);
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity 0.5.16;
+//pragma solidity >=0.4.21 <0.7.0; this is the defualt
 
 contract SimpleStorage {
   uint storedData;
@@ -9,5 +10,11 @@ contract SimpleStorage {
 
   function get() public view returns (uint) {
     return storedData;
+  }
+
+  address payable[] recipients;
+  function sendEther(address payable recipient) external {
+    recipient.transfer(11 ether);
+    //we need to do the algo for amount
   }
 }
