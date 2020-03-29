67b69eb42aeaea782852eac4d21cc6592b55aaa3
Markos Georghiades <53157953+Markos-The-G@users.noreply.github.com>
2020-03-29 06:33:34 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -94,18 +94,18 @@ class App extends Component {
     }
   };
 
-  // runExample = async () => {
-  //   const { accounts, contract } = this.state;
+  runExample = async () => {
+    const { accounts, contract } = this.state;
 
-  //   // Stores a given value, 5 by default.
-  //   await contract.methods.set(5).send({ from: accounts[0] });
+    // Stores a given value, 5 by default.
+    await contract.methods.set(5).send({ from: accounts[0] });
 
-  //   // Get the value from the contract to prove it worked.
-  //   const response = await contract.methods.get().call();
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.get().call();
 
-  //   // Update state with the result.
-  //   this.setState({ storageValue: response });
-  // };
+    // Update state with the result.
+    this.setState({ storageValue: response });
+  };
   async testing(){
     
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -293,7 +293,7 @@ class App extends Component {
                     component="label"
                     color="primary"
                     className="upload-button"
-                    onClick={() => this.testing()}
+                    onClick={() => this.runExample()}
                     >
                       <div className="receievePayment">Receive Payment</div>
                 </Button>
